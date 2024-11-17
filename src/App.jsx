5b0f29433681c0b4a62461251448e7a36cc36757
ExonegeS <2004gusak@gmail.com>
2024-11-18 02:40:34 +0500

    "Added handleAddTodos function to App component, passed it as prop to TodoInput, and updated TodoInput to use state and call handleAddTodos on button click."

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import TodoList from "./components/TodoList"
 
 function App() {
     // null default value
-    const { todos, setTodos} = useState([
+    const [ todos, setTodos ] = useState([
         'Go to the gym',
         'Eat more fruits and vege',
         'Learn React',
     ])
 
+    function handleAddTodos(newTodo) {
+        const newTodoList = [...todos, newTodo]
+        setTodos(newTodoList)
+    }
+
     return (
         <>
-            <TodoInput />
+            <TodoInput handleAddTodos={handleAddTodos}/>
             <TodoList todos={todos}/>
         </>
     )
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import TodoCard from './TodoCard'
 
 export default function TodoList(props) {
-    const {todos} = props
-
+    const { todos } = props
+    
     return (
         <ul className='main'>
             {todos.map((todo, todoIndex) => {
